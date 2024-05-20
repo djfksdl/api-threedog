40c49fd81b80005c1e454171e573016274c3d3a2
aassdd236 <aassdd236@naver.com>
2024-05-20 14:16:45 +0900

    qwe

--- src/main/java/com/javaex/dao/YEDao.java
+++ src/main/java/com/javaex/dao/YEDao.java
@@ -3,11 +3,36 @@ package com.javaex.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.javaex.vo.BusinessVo;
 
 @Repository
 public class YEDao {
 
 	@Autowired
 	private SqlSession sqlSession;
+
+	public void msignup(BusinessVo businessVo) {
+		System.out.println("YEDao.msignup()");
+		
+		sqlSession.insert("ye.bInsert", businessVo);
+	}
+
+	public int idCheck(String id) {
+		System.out.println("YEDao.idCheck()");
+		
+		int count = sqlSession.selectOne("ye.bSelect", id);
+		
+		return count;
+	}
+
+	public BusinessVo mlogin(@ModelAttribute BusinessVo businessVo) {
+		System.out.println("YEDao.mlogin()");
+		
+		BusinessVo auth = sqlSession.selectOne("ye.bSelectOne", businessVo);
+		
+		return auth;
+	}
 
 }
--- src/main/java/com/javaex/service/YEService.java
+++ src/main/java/com/javaex/service/YEService.java
@@ -2,13 +2,37 @@ package com.javaex.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.javaex.dao.YEDao;
+import com.javaex.vo.BusinessVo;
 
 @Service
 public class YEService {
 
 	@Autowired
 	private YEDao yeDao;
+
+	public void exeMsignup(BusinessVo businessVo) {
+		System.out.println("YEService.exeMsignup()");
+		
+		yeDao.msignup(businessVo);
+	}
+
+	public int exeIdCheck(String id) {
+		System.out.println("YEService.exeIdCheck()");
+		
+		int count = yeDao.idCheck(id);
+		
+		return count;
+	}
+
+	public BusinessVo exeMlogin(@ModelAttribute BusinessVo businessVo) {
+		System.out.println("YEService.exeMlogin()");
+		
+		BusinessVo auth = yeDao.mlogin(businessVo);
+		
+		return auth;
+	}
 	
 }
