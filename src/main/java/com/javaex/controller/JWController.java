8122df9f4ff03c1b248df882dec6fd267bca635d
ojiwon99 <dhwldnjs0420@naver.com>
2024-05-23 14:40:16 +0900

    지원이 우하하하하 드래그 했다

--- src/main/java/com/javaex/controller/JWController.java
+++ src/main/java/com/javaex/controller/JWController.java
@@ -3,6 +3,7 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,35 +17,60 @@ import com.javaex.vo.ReserveVo;
 @RestController
 public class JWController {
 
-    @Autowired
-    private JWService jwService;
+	@Autowired
+	private JWService jwService;
 
-    // 한 가게의 예약 리스트 정보 조회 
-    @GetMapping("/api/jw/{bNo}")
-    public JsonResult selectReserveList(@PathVariable("bNo") int bNo) {
-        System.out.println("JWController.list");
-    
+	// 한 가게의 예약 리스트 정보 조회
+	@GetMapping("/api/jw/{bNo}")
+	public JsonResult selectReserveList(@PathVariable("bNo") int bNo) {
+		System.out.println("JWController.list");
 
-        List<ReserveVo> reserveList = jwService.getReserveList(bNo);
-        System.out.println(reserveList);
+		List<ReserveVo> reserveList = jwService.getReserveList(bNo);
+		System.out.println(reserveList);
 
-        return JsonResult.success(reserveList);
-    }
+		return JsonResult.success(reserveList);
+	}
 
-    // 예약 정보 업데이트
-    @PutMapping("/api/jw/{rsNo}")
-    public JsonResult updateReserve(@PathVariable("rsNo") int rsNo, @RequestBody ReserveVo reserveVo) {
-        System.out.println("JWController.updateReserve");
+	// 일정 수정
+	@PutMapping("/api/jw/{rsNo}/date")
+	public JsonResult updateReserveDate(@PathVariable("rsNo") int rsNo, @RequestBody ReserveVo reserveVo) {
+		reserveVo.setRsNo(rsNo);
+		System.out.println("일정 수정 메서드 실행");
 
-        reserveVo.setRsNo(rsNo);
-        System.out.println(reserveVo);
+		jwService.updateReserveDate(reserveVo);
+		return JsonResult.success(reserveVo);
+	}
 
-        int count = jwService.exeUpdateReserve(reserveVo);
+	// 시간 수정
+	@PutMapping("/api/jw/{rsNo}/time")
+	public JsonResult updateReserveTime(@PathVariable("rsNo") int rsNo, @RequestBody ReserveVo reserveVo) {
+		reserveVo.setRsNo(rsNo);
+		System.out.println("시간 수정 메서드 실행");
+		jwService.updateReserveTime(reserveVo);
+		return JsonResult.success(reserveVo);
+	}
 
-        if (count > 0) {
-            return JsonResult.success(reserveVo);
-        } else {
-            return JsonResult.fail("Update failed");
-        }
-    }
+	// 일정 삭제
+	@DeleteMapping("/api/jw/{rsNo}")
+	public JsonResult deleteReserve(@PathVariable("rsNo") int rsNo) {
+		jwService.deleteReserve(rsNo);
+		return JsonResult.success(rsNo);
+	}
+
+	// 예약 정보 업데이트
+	@PutMapping("/api/jw/diary/{rsNo}")
+	public JsonResult updateReserve(@PathVariable("rsNo") int rsNo, @RequestBody ReserveVo reserveVo) {
+		System.out.println("JWController.updateReserve");
+
+		reserveVo.setRsNo(rsNo);
+		System.out.println(reserveVo);
+
+		int count = jwService.exeUpdateReserve(reserveVo);
+
+		if (count > 0) {
+			return JsonResult.success(reserveVo);
+		} else {
+			return JsonResult.fail("Update failed");
+		}
+	}
 }
--- src/main/java/com/javaex/dao/JWDao.java
+++ src/main/java/com/javaex/dao/JWDao.java
@@ -11,26 +11,43 @@ import com.javaex.vo.ReserveVo;
 @Repository
 public class JWDao {
 
-    @Autowired
-    private SqlSession sqlSession;
-    
+	@Autowired
+	private SqlSession sqlSession;
 
-    // 한 가게의 예약 리스트 정보 조회 
-    public List<ReserveVo> selectReserveList(int bNo) {
-        System.out.println("JWDao.selectReserveList()");
+	// 한 가게의 예약 리스트 정보 조회
+	public List<ReserveVo> selectReserveList(int bNo) {
+		System.out.println("JWDao.selectReserveList()");
 
-        List<ReserveVo> reserveList = sqlSession.selectList("jw.selectReserveList", bNo);
-//        System.out.println("fdefg"+reserveList);
-        
-        return reserveList;
-    }
+		List<ReserveVo> reserveList = sqlSession.selectList("jw.selectReserveList", bNo);
+		// System.out.println("fdefg"+reserveList);
 
-    // 예약 정보 업데이트
-    public int updateReserve(ReserveVo reserveVo) {
-        System.out.println("JWDao.updateReserve()");
+		return reserveList;
+	}
 
-        int count = sqlSession.update("jw.updateReserve", reserveVo);
-        System.out.println(count);
-        return count;
-    }
+	// 일정 수정
+	public void updateReserveDate(ReserveVo reserveVo) {
+		sqlSession.update("jw.updateReserveDate", reserveVo);
+		System.out.println("예약 일정이 수정되었습니다.");
+	}
+
+	// 시간 수정
+	public void updateReserveTime(ReserveVo reserveVo) {
+		sqlSession.update("jw.updateReserveTime", reserveVo);
+		System.out.println("예약 시간이 수정되었습니다.");
+	}
+
+	// 예약 삭제
+	public void deleteReserve(int rsNo) {
+		sqlSession.delete("jw.deleteReserve", rsNo);
+	}
+
+	// 예약 정보 업데이트
+	public int updateReserve(ReserveVo reserveVo) {
+		System.out.println("JWDao.updateReserve()");
+
+		int count = sqlSession.update("jw.updateReserve", reserveVo);
+		System.out.println(count);
+		return count;
+	}
+
 }
